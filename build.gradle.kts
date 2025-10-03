ab6b50ff740019f7b3c8ebcd9b981f6d28fc0df2
Naresh NK <nareshnath.nn@gmail.com>
2025-10-03 16:01:27 +0530

    Added git pre-commit hook

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,3 +5,27 @@ plugins {
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.kotlinter) apply false
 }
+
+tasks.register("copyGitHooks", Copy::class.java) {
+    println("Copying git hooks...")
+    description = "Copies the git hooks from /git-hooks to the .git folder."
+    group = "git hooks"
+    from("$rootDir/scripts/pre-commit")
+    into("$rootDir/.git/hooks/")
+}
+tasks.register("installGitHooks", Exec::class.java) {
+    println("Installing git hooks...")
+    description = "Installs the pre-commit git hooks from /git-hooks."
+    group = "git hooks"
+    workingDir = rootDir
+    commandLine = listOf("chmod")
+    args("-R", "+x", ".git/hooks/")
+    dependsOn("copyGitHooks")
+    doLast {
+        println("Git hook installed successfully.")
+    }
+}
+
+afterEvaluate {
+    tasks.getByPath(":app:preBuild").dependsOn(":installGitHooks")
+}
