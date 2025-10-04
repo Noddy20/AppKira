c4789bf3bff36bd84c84a1553ef931f21e513d5e
Naresh NK <nareshnath.nn@gmail.com>
2025-10-04 12:35:31 +0530

    Added bottom nav and screens

--- app/src/main/java/com/nnk/appkira/presentation/screens/HomeActivity.kt
+++ app/src/main/java/com/nnk/appkira/presentation/screens/HomeActivity.kt
@@ -4,14 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.nnk.appkira.presentation.designsystem.theme.AppKiraTheme
+import com.nnk.appkira.presentation.screens.home.HomeContainerScreen
 
 class HomeActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,32 +13,8 @@ class HomeActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AppKiraTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding),
-                    )
-                }
+                HomeContainerScreen()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(
-    name: String,
-    modifier: Modifier = Modifier,
-) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier,
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AppKiraTheme {
-        Greeting("Android")
     }
 }
