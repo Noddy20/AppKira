00e10c5cdc8be89a8e87886154610a38fce5b6b0
Naresh NK <nareshnath.nn@gmail.com>
2025-10-13 17:30:52 +0530

    Added intro screen container with pager

--- app/src/main/java/com/nnk/appkira/presentation/features/intro/IntroActivity.kt
+++ app/src/main/java/com/nnk/appkira/presentation/features/intro/IntroActivity.kt
@@ -1,10 +1,14 @@
 package com.nnk.appkira.presentation.features.intro
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.nnk.appkira.presentation.designsystem.theme.AppKiraTheme
+import com.nnk.appkira.presentation.features.home.HomeActivity
+import com.nnk.appkira.presentation.features.intro.screens.IntroContainerScreen
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -13,8 +17,16 @@ class IntroActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            AppKiraTheme {
+            AppKiraTheme(darkTheme = true) {
+                IntroContainerScreen {
+                    startActivity(HomeActivity.getIntent(this))
+                    finish()
+                }
             }
         }
+    }
+
+    companion object {
+        fun getIntent(context: Context): Intent = Intent(context, IntroActivity::class.java)
     }
 }
