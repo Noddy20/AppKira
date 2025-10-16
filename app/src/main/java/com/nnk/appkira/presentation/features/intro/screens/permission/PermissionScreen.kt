5da549eb0aba8fc34d6c4730b52ed16798a6b64b
Naresh NK <nareshnath.nn@gmail.com>
2025-10-16 21:06:07 +0530

    Added intro permission screen usage state permission button

--- app/src/main/java/com/nnk/appkira/presentation/features/intro/screens/permission/PermissionScreen.kt
+++ app/src/main/java/com/nnk/appkira/presentation/features/intro/screens/permission/PermissionScreen.kt
@@ -1,11 +1,17 @@
 package com.nnk.appkira.presentation.features.intro.screens.permission
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -36,7 +42,53 @@ fun PermissionScreen() {
             style = MaterialTheme.typography.bodyLarge,
             color = MaterialTheme.colorScheme.primary,
         )
+
+        HorizontalDivider(
+            modifier = Modifier.padding(top = AppDimen.Dimen10X),
+            thickness = AppDimen.Dimen1,
+            color = MaterialTheme.colorScheme.onSurface,
+        )
+
+        PermissionButton(
+            text = stringResource(R.string.permission_usage_access),
+            isChecked = false,
+        ) {
+        }
     }
+}
+
+@Composable
+private fun PermissionButton(
+    text: String,
+    isChecked: Boolean,
+    onCheckedChange: (Boolean) -> Unit,
+) {
+    Row(
+        modifier =
+            Modifier
+                .fillMaxWidth()
+                .padding(
+                    horizontal = AppDimen.Dimen4X,
+                    vertical = AppDimen.Dimen4X,
+                ),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween,
+    ) {
+        Text(
+            text = text,
+            style = MaterialTheme.typography.titleMedium,
+            color = MaterialTheme.colorScheme.primary,
+        )
+        Switch(
+            modifier = Modifier,
+            checked = isChecked,
+            onCheckedChange = onCheckedChange,
+        )
+    }
+    HorizontalDivider(
+        thickness = AppDimen.Dimen1,
+        color = MaterialTheme.colorScheme.onSurface,
+    )
 }
 
 @Preview
