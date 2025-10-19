2c20403830d7e2d54674726a1c8e9bd306cb29c8
Naresh NK <nareshnath.nn@gmail.com>
2025-10-19 12:10:13 +0530

    Added permission screen launch on home when permission not granted

--- app/src/main/java/com/nnk/appkira/presentation/features/home/HomeActivity.kt
+++ app/src/main/java/com/nnk/appkira/presentation/features/home/HomeActivity.kt
@@ -6,19 +6,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.compose.LocalLifecycleOwner
 import androidx.lifecycle.lifecycleScope
 import com.nnk.appkira.core.storage.AppPreferences
+import com.nnk.appkira.core.system.PermissionManager
+import com.nnk.appkira.presentation.designsystem.dimen.AppDimen
 import com.nnk.appkira.presentation.designsystem.theme.AppKiraTheme
 import com.nnk.appkira.presentation.features.home.screens.HomeContainerScreen
 import com.nnk.appkira.presentation.features.intro.IntroActivity
+import com.nnk.appkira.presentation.features.intro.screens.permission.PermissionScreen
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -27,6 +33,9 @@ import javax.inject.Inject
 class HomeActivity : ComponentActivity() {
     @Inject
     lateinit var appPreferences: AppPreferences
+
+    @Inject
+    lateinit var permissionManager: PermissionManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,10 +52,24 @@ class HomeActivity : ComponentActivity() {
                     }
                 }
 
+                val lifecycleState by lifecycleOwner.lifecycle.currentStateFlow.collectAsState()
+                var isUsagePermissionGranted by remember { mutableStateOf(permissionManager.isUsageStatePermissionGranted()) }
+
+                LaunchedEffect(lifecycleState) {
+                    if (lifecycleState == Lifecycle.State.RESUMED) {
+                        isUsagePermissionGranted = permissionManager.isUsageStatePermissionGranted()
+                    }
+                }
+
                 when {
                     isAppIntroShown.not() -> {
                         startActivity(IntroActivity.getIntent(this))
                         finish()
+                    }
+                    isUsagePermissionGranted.not() -> {
+                        Box(modifier = Modifier.padding(vertical = AppDimen.Dimen10X)) {
+                            PermissionScreen(permissionManager)
+                        }
                     }
                     else -> {
                         HomeContainerScreen()
