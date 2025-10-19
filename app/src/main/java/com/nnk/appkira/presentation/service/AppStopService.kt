1a473bf0f416ed4328d032fa995675f42dd945a6
Naresh NK <nareshnath.nn@gmail.com>
2025-10-19 20:29:30 +0530

    Added AccessibilityService for app force stop

---
+++ app/src/main/java/com/nnk/appkira/presentation/service/AppStopService.kt
@@ -0,0 +1,17 @@
+package com.nnk.appkira.presentation.service
+
+import android.accessibilityservice.AccessibilityService
+import android.annotation.SuppressLint
+import android.view.accessibility.AccessibilityEvent
+
+@SuppressLint("AccessibilityPolicy")
+class AppStopService : AccessibilityService() {
+
+    override fun onAccessibilityEvent(p0: AccessibilityEvent?) {
+
+    }
+
+    override fun onInterrupt() {
+
+    }
+}
