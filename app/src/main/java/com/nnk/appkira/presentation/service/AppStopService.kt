3b8a9c0faf3e028a59f3f308eb7c57ac627d8669
Naresh NK <nareshnath.nn@gmail.com>
2025-10-20 11:03:58 +0530

    Fixed lint issues

--- app/src/main/java/com/nnk/appkira/presentation/service/AppStopService.kt
+++ app/src/main/java/com/nnk/appkira/presentation/service/AppStopService.kt
@@ -6,12 +6,9 @@ import android.view.accessibility.AccessibilityEvent
 
 @SuppressLint("AccessibilityPolicy")
 class AppStopService : AccessibilityService() {
-
     override fun onAccessibilityEvent(p0: AccessibilityEvent?) {
-
     }
 
     override fun onInterrupt() {
-
     }
 }
