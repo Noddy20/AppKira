88b2af63a21c3d64c4347e77f9059c27ad0c2ba7
Naresh NK <nareshnath.nn@gmail.com>
2025-10-20 18:55:12 +0530

    Added Dashboard screen Apps list info item UI

--- app/src/main/java/com/nnk/appkira/presentation/features/home/screens/dashboard/DashboardScreen.kt
+++ app/src/main/java/com/nnk/appkira/presentation/features/home/screens/dashboard/DashboardScreen.kt
@@ -1,33 +1,86 @@
 package com.nnk.appkira.presentation.features.home.screens.dashboard
 
-import android.app.AppOpsManager
-import android.content.Context
-import android.content.Intent
-import android.content.pm.PackageManager
-import android.net.Uri
-import android.provider.Settings
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.List
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
-import androidx.core.content.ContextCompat
-import com.nnk.appkira.core.system.PermissionManager
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.nnk.appkira.R
+import com.nnk.appkira.presentation.designsystem.dimen.AppDimen
+import com.nnk.appkira.presentation.designsystem.theme.AppKiraTheme
 
 @Composable
 fun DashboardScreen() {
-    val context = LocalContext.current
+    Column(
+        modifier =
+            Modifier
+                .fillMaxSize(),
+        verticalArrangement = Arrangement.spacedBy(AppDimen.DimenX),
+    ) {
+        HorizontalDivider(
+            modifier = Modifier,
+            thickness = AppDimen.Dimen1,
+            color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.1f),
+        )
+        ListItem(
+            icon = Icons.AutoMirrored.Default.List,
+            text = stringResource(R.string.apps_list),
+        )
+    }
+}
 
-    Column {
-        Text("Dashboard Screen")
+@Composable
+private fun ListItem(
+    icon: ImageVector,
+    text: String,
+) {
+    Row(
+        modifier =
+            Modifier
+                .fillMaxWidth()
+                .padding(
+                    horizontal = AppDimen.Dimen4X,
+                    vertical = AppDimen.Dimen5X,
+                ),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(AppDimen.Dimen4X),
+    ) {
+        Icon(
+            imageVector = icon,
+            contentDescription = stringResource(R.string.content_desc_info_list_icon, text),
+            tint = MaterialTheme.colorScheme.onBackground,
+        )
+        Text(
+            text = text,
+            style = MaterialTheme.typography.titleMedium,
+            color = MaterialTheme.colorScheme.onBackground,
+        )
+    }
+    HorizontalDivider(
+        modifier = Modifier,
+        thickness = AppDimen.Dimen1,
+        color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.1f),
+    )
+}
 
-        Button(onClick = {
-            val pm = PermissionManager.getInstance(context)
-            if (pm.isUsageStatePermissionGranted().not()) {
-                pm.launchUsageAccessSettings()
-            }
-        }) {
-            Text("Request Usage Stats Permission")
-        }
+@Preview
+@Composable
+private fun PreviewDashboardScreen() {
+    AppKiraTheme {
+        DashboardScreen()
     }
 }
