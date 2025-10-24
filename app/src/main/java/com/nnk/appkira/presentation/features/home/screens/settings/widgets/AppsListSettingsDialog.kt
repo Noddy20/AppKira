008e5ab598cbf2b4ed3c84fcb793f5beb7c22900
Naresh NK <nareshnath.nn@gmail.com>
2025-10-24 20:35:52 +0530

    Renamed Dashboard screen to settings screen

--- app/src/main/java/com/nnk/appkira/presentation/features/home/screens/settings/widgets/AppsListSettingsDialog.kt
+++ app/src/main/java/com/nnk/appkira/presentation/features/home/screens/settings/widgets/AppsListSettingsDialog.kt
@@ -1,6 +1,5 @@
-package com.nnk.appkira.presentation.features.home.screens.dashboard.widgets
+package com.nnk.appkira.presentation.features.home.screens.settings.widgets
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
